9ef5bb06f2b6000048d877d4d3ecc2af58d5d371
evandrodametto <evandrotdametto@gmail.com>
2021-03-13 14:17:28 -0300

    exe1

---
+++ exercicio1.py
@@ -0,0 +1,7 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+
+# Seu código aqui
+
+print('Seu nome é:', nome)
+print('Sua idade daqui cinco anos será:', (int(idade) + 5))
